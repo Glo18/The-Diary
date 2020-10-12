dbf38c353513e8ced51db831345fa37ad003c48c
Glo18 <kiogoraspecial@gmail.com>
2020-10-12 11:16:15 +0300

    add contact form information

--- Desktop/Delani-Studio/js/scripts.js
+++ Desktop/Delani-Studio/js/scripts.js
@@ -9,3 +9,24 @@ $(document).ready(function() {
         $("#click3").toggle();
     });
 });
+var btn = document.getElementById("mySubw");
+
+btn.onclick = function(event)
+    var x = document.forms["myForm"]["contact-name"].value;
+    if (x == "") {
+        alert("Name not entered");
+        return false;
+    }
+    var y = document.forms["myForm"]["contact-email"].value;
+    if (y == "") {
+        alert("Email not entered");
+        return false;
+    }
+    var z = document.forms["myForm"]["contact-message"].value;
+    if (z == "") {
+        alert("You have no message to send");
+        return false;
+    }
+    var person1Input = $("input#contact-name").val();
+    $("#outputName").append(person1Input);
+
