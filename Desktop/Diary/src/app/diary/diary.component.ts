85b55d1f98ef48b0442b6d0d935a6abbbba88e71
Glo18 <kiogoraspecial@gmail.com>
2020-11-06 00:20:08 +0300

    create class and component

---
+++ Desktop/Diary/src/app/diary/diary.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Diary } from '../diary';
+
+@Component({
+  selector: 'app-diary',
+  templateUrl: './diary.component.html',
+  styleUrls: ['./diary.component.css']
+})
+export class DiaryComponent implements OnInit {
+
+  diary = 'Privacy!';
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
