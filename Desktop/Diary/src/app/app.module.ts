0a6ce94860af75400343d72e7b7d66bdd318a6bc
Glo18 <kiogoraspecial@gmail.com>
2020-11-06 01:36:07 +0300

    add child component

--- Desktop/Diary/src/app/app.module.ts
+++ Desktop/Diary/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiaryComponent } from './diary/diary.component';
+import { DiaryDetailComponent } from './diary-detail/diary-detail.component';
 
 @NgModule({
   declarations: [
-  DiaryComponent],
+  DiaryComponent,
+  DiaryDetailComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
